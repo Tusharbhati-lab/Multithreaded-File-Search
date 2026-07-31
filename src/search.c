abe35012a26c3408ebaf3c20509d771f8b7a293c
Tusharbhati-lab <bhatitushar143@gmail.com>
2026-07-31 23:15:43 +0530

    Implemented basic directory traversal using dirent

--- src/search.c
+++ src/search.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+
 #include "search.h"
 
-void searchDirectory(const char *directory, const char *targetFile)
+void searchDirectory(const char *directory,
+                     const char *targetFile)
 {
-    printf("Directory: %s\n", directory);
-    printf("Target File: %s\n", targetFile);
+    DIR *dir;
 
-    printf("\nSearch feature coming soon...\n");
+    struct dirent *entry;
+
+    dir = opendir(directory);
+
+    if(dir == NULL)
+    {
+        printf("Cannot open directory.\n");
+        return;
+    }
+
+    while((entry = readdir(dir)) != NULL)
+    {
+
+        printf("%s\n", entry->d_name);
+
+    }
+
+    closedir(dir);
 }
